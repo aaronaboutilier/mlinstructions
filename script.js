d67c82da2d8288e36d5bc9c31e596dc2db9aa0d6
Aaron Boutilier <aaronaboutilier@gmail.com>
2026-05-20 18:34:26 -0300

    updates

--- script.js
+++
@@ -1,8 +0,0 @@
-const helloBtn = document.getElementById("hello-btn");
-const helloOutput = document.getElementById("hello-output");
-
-if (helloBtn && helloOutput) {
-  helloBtn.addEventListener("click", () => {
-    helloOutput.textContent = "Hello from vanilla JavaScript.";
-  });
-}
